d40ad6081198ce7340099bd3df3375abad806227
Stephen <datfoosteve@gmail.com>
2023-05-30 00:39:28 -0500

    i guess

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Converse with multiple PDFs",
+    st.set_page_config(page_title="ETHOS AI PDF Chatbot",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,20 +1,21 @@
 css = '''
 <style>
+
 .chat-message {
     padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
 }
 .chat-message.user {
     background-color: #2b313e
 }
---------------------
+
 .chat-message.user .message { 
-    background-color: #4f5b66
+    background-color: #D1795E
 }
 
 .chat-message.bot .message {
-    background-color: #475063
+    background-color: #D1795E
 }
---------------------
+
 
 .chat-message.bot {
     background-color: #475063 
@@ -38,7 +39,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://em-content.zobj.net/source/microsoft-teams/363/robot_1f916.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -47,7 +48,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://ibb.co/3RMG6Dj" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://em-content.zobj.net/source/microsoft-teams/363/beaming-face-with-smiling-eyes_1f601.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
