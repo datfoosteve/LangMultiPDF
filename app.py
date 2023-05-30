a37214dfe9c6e33fd46f6f01df57aa8d3a6c8bc0
Stephen <datfoosteve@gmail.com>
2023-05-29 19:27:59 -0500

    Lol just tiny little edits

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
+    st.set_page_config(page_title="Converse with multiple PDFs",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
@@ -76,7 +76,7 @@ def main():
         st.session_state.chat_history = None
 
     st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
+    user_question = st.text_input("Ask a question about your PDF Documents:")
     if user_question:
         handle_userinput(user_question)
 
